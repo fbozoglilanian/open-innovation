f66e52725a85bb8eb148d60621257cf090443df6
unknown <fabian.bozoglian@gmail.com>
2013-07-21 17:59:17 -0300

    Design structure

---
+++ module/Design/config/module.config.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+		'router' => array(
+				'routes' => array(
+						'design' => array(
+								'type' => 'Zend\Mvc\Router\Http\Literal',
+								'options' => array(
+										'route'    => '/design',
+										'defaults' => array(
+												'controller' => 'Design\Controller\Index',
+												'action'     => 'index',
+										),
+								),
+						),
+				),
+		),
+		'controllers' => array(
+				'invokables' => array(
+						'Design\Controller\Challenge' => 'Design\Controller\ChallengeController',
+						'Design\Controller\Index' => 'Design\Controller\IndexController',
+				),
+		),
+		'view_manager' => array(
+				'template_path_stack' => array(
+						'album' => __DIR__ . '/../view',
+				),
+		),
+);
